720bfbd08d0c943e2103208dd5bc6f576bf7e64b
Sujata Yeole <divyary136@gmail.com>
2025-08-12 10:09:28 +0530

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Smart_Ride.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
